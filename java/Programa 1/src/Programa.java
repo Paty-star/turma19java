dafd3bf990503ac856d827c7aac9b8d90189af96
Paty-star <paty.nascimento.94@gmail.com>
2021-04-06 21:10:15 -0300

    Exercícios

---
+++ java/Programa 1/src/Programa.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+	
+public static void main (String[] args)	{
+	
+	System.out.println("oi");
+	System.out.println(0);
+	Scanner leia= new Scanner
+			public static void parImpar(int numero) {
+		
+ int numero;
+ System.out.println("digite um numero:");
+ numero = leia.next()
+		 if ((numero%2)==0&& numero>0)
+		 {
+		 System.out.println("numero par");
+		 }
+		 else if ((numero%2)==1 && numero>0)
+		 {
+			 System.out.println("numero impar ");
+		 }
+		parImpar(3);
+		
+	}
+}
+ 
+
